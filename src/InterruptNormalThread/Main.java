5a6215c3aaedb96e9a62b70529aa0d8016a96288
bala <sr.bala.kkl@gmail.com>
2022-02-16 09:01:46 +0530

    add : thread Interrupt with normal method and isInterrupt()

---
+++ src/InterruptNormalThread/Main.java
@@ -0,0 +1,39 @@
+package InterruptNormalThread;
+import static java.lang.Thread.currentThread;
+
+class Loader implements Runnable {
+    @Override
+    public void run() {
+//    currentThread().getName() -> to get a name of current thread
+
+//        System.out.println("hello from ->" + currentThread().getName());
+//        try {
+//            System.out.println("hi i'm a from try block \n");
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+////            e.printStackTrace();
+//            System.out.println("Thread is Interupted");
+//        }
+//
+//        System.out.println("Loader Thread: Seconds have passed I'm awake");
+
+        for (int i = 0; i < 5; i++)
+            System.out.println(i);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("from main thread.");
+        Thread rv = new Thread(new Loader());
+//        changing default name of thread
+        rv.setName(" ==Loader Thread==");
+        rv.start();
+        rv.interrupt();
+        System.out.println("hello again from main");
+
+        System.out.println("thread interrupt flag = "+rv.isInterrupted());
+
+    }
+}
+
--- src/ThreadIterrupt/Main.java
+++ src/ThreadIterrupt/Main.java
@@ -4,28 +4,31 @@ import static java.lang.Thread.currentThread;
 
 class Loader implements Runnable {
     @Override
-//    currentThread().getName() -> to get a name of current thread
     public void run() {
+//    currentThread().getName() -> to get a name of current thread
+
         System.out.println("hello from ->" + currentThread().getName());
         try {
+            System.out.println("hi i'm a from try block \n");
             Thread.sleep(3000);
         } catch (InterruptedException e) {
 //            e.printStackTrace();
-            System.out.println("Thread is Interupted");
-
+            System.out.println("Thread is Interupted" );
         }
+
         System.out.println("Loader Thread: Seconds have passed I'm awake");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("from main thread.");
+        System.out.println("from main mtd.");
         Thread rv = new Thread(new Loader());
 //        changing default name of thread
         rv.setName(" ==Loader Thread==");
         rv.start();
         rv.interrupt();
         System.out.println("hello again from main");
+        System.out.println("Thread interruption status = "+rv.isInterrupted());
     }
 }
